e06276756baf8f167f66a67bcbfa80efe757d306
Rafael José do Nascimento <eng.rafaeljosedonascimento@gmail.com>
2023-04-16 01:34:22 -0300

    Jitsi and UI configured in react native app

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
+    TextInput,
     TouchableOpacity,
     StyleSheet,
     NativeModules,
@@ -9,20 +10,42 @@ import {
 
 const App = () => {
 
+    const { OpenActivity } = NativeModules;
 
-    const {OpenActivity} = NativeModules;
+    const [conferenceUrl, onChangeconferenceUrl] = useState('');
+    const [conferenceName, onChangeConferenceName] = useState('');
 
     const onPressOpen = () => {
-        OpenActivity.open();
+        let url = '';
+        if(conferenceName.length !== 0) {
+            url = `https://meet.jit.si/${conferenceName}`;
+        } else {
+            url = conferenceUrl;
+        }
+        if(url.length > 0) {
+            OpenActivity.open(url);
+        }
     }
 
     return (
         <View style={styles.container}>
+            <TextInput
+                style={[styles.input, {marginBottom: 20}]}
+                placeholder='conference url'
+                onChangeText={onChangeconferenceUrl}
+                value={conferenceUrl}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='conference name'
+                onChangeText={onChangeConferenceName}
+                value={conferenceName}
+            />
             <TouchableOpacity
                 onPress={() => onPressOpen()}
                 style={styles.button}>
                 <Text style={{ color: 'white' }}>
-                    Abrir atividade
+                    open conference
                 </Text>
             </TouchableOpacity>
         </View>
@@ -41,7 +64,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#000000'
-    }
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        marginBottom: 100,
+        borderWidth: 1,
+        padding: 10,
+    },
 });
 
 export default App;
